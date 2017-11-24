dd0fe4ef51e1689daed3e63ed010d5e8fd1b0df1
Damien <dtohin@outlook.fr>
2017-11-24 22:30:43 +0100

    basic application start template

---
+++ MyCodeCamp/Controllers/CampsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyCodeCamp.Controllers
+{
+    [Route("api/camps")]
+    public class CampsController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Get()
+        {
+            return Ok(new { Name = "Damien", FavoriteColor = "Blue" });
+        }
+
+
+    }
+}
